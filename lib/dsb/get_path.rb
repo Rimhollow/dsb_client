3de88462f94f73dd501aa8c56fb07de472fb5c7e
Robert Holland <rmholl0@uky.edu>
2013-06-05 15:07:00 -0400

    Expand Get Path task and config file to generate ingest_path as well as processing_path

--- lib/dsb/get_path.rb
+++ lib/dsb/get_path.rb
@@ -9,10 +9,14 @@ module Dsb
 
     def perform_task
       processing_path = File.join(@client.processing_directory,
+                                  @package.get(:identifier))
+      ingest_path = File.join(@client.ingest_directory,
                               @package.get(:identifier))
       @package.set(:processing_path, processing_path)
+      @package.set(:ingest_path, ingest_path)
       @package.save
-      if @package.get(:processing_path) == processing_path
+      if (@package.get(:processing_path) == processing_path) and
+         (@package.get(:ingest_path) == ingest_path)
         @task.complete!
       else
         @task.fail!
