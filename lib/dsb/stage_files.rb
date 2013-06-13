2c5d9894b70c78b95b7184f73d8494afec0ddc2c
Robert Holland <rmholl0@uky.edu>
2013-06-13 13:24:01 -0400

    Remove unused variable

--- lib/dsb/stage_files.rb
+++ lib/dsb/stage_files.rb
@@ -11,7 +11,6 @@ module Dsb
       source_path = File.absolute_path(@package.get(:source_path))
       processing_path = File.absolute_path(@package.get(:processing_path))
       FileUtils.mkdir_p processing_path
-      processing_directory = File.absolute_path(@client.processing_directory)
       if File.directory?(source_path) and File.directory?(processing_path)
         # XXX Repeat the following until it stabilizes
         if system(@client.rsync_command, @client.rsync_options,
